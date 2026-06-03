303803b3e6b566d92f066e4b9cb7e39eac42c82b
YUKI ENDO <stb5181@gmail.com>
2026-06-03 23:54:07 +0900

    Configure Simple Form and I18n

--- spec/requests/ports_spec.rb
+++ spec/requests/ports_spec.rb
@@ -3,16 +3,9 @@ require 'rails_helper'
 RSpec.describe "Ports", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/ports/index"
+      get ports_path
+
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/ports/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
