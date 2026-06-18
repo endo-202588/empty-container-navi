9de3ba0c99c73b4caea21f76130083c3748b94e7
endo-202588 <stb5181@gmail.com>
2026-06-18 16:20:05 +0900

    fix lint check

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -27,7 +27,7 @@ class BookingsController < ApplicationController
 
     if @booking.save
       BookingMailer.completed(@booking).deliver_now
-      
+
       redirect_to @booking, success: "予約を登録しました"
     else
       render :new, status: :unprocessable_content
